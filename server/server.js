4455b3b598975b3011d57c2f4b05996ddce61e1c
Deep05-tech <vdipbhai551@rku.ac.in>
2026-05-31 22:04:42 +0530

    Fix streaming format issue by switching player client to android,web

--- server/server.js
+++ server/server.js
@@ -442,7 +442,7 @@ app.get("/stream/:videoId", (req, res) => {
     "--get-url",
     "--no-warnings",
     "--no-playlist",
-    "--extractor-args", "youtube:player_client=mweb,tv;player_skip=webpage",
+    "--extractor-args", "youtube:player_client=android,web;player_skip=webpage",
     "--no-check-certificate",
     "--js-runtimes", `node:${process.execPath}`,
     ...getCookiesArgs(),
@@ -483,7 +483,7 @@ app.get("/debug-ytdlp", (req, res) => {
   const localStats = localExists ? fs.statSync(localYtDlpPath) : null;
 
   if (videoId) {
-    const spoofClient = client || "mweb,tv";
+    const spoofClient = client || "android,web";
     const format = req.query.format || "bestaudio/best";
     const args = [
       `https://www.youtube.com/watch?v=${videoId}`,
